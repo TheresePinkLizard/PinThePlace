21d6a089395e74f13eccc8287f2e7f1446e5953f
97illimo <114436711+97illimo@users.noreply.github.com>
2024-10-16 13:50:20 +0200

    Oppdaterte User model og databasen

--- PinThePlace/Migrations/20241016114754_ShopDbExpanded.Designer.cs
+++ PinThePlace/Migrations/20241016114754_ShopDbExpanded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PinThePlace.Models;
 
@@ -10,9 +11,11 @@ using PinThePlace.Models;
 namespace PinThePlace.Migrations
 {
     [DbContext(typeof(PinDbContext))]
-    partial class PinDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241016114754_ShopDbExpanded")]
+    partial class ShopDbExpanded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
---
+++ PinThePlace/Migrations/20241016114754_ShopDbExpanded.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PinThePlace.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShopDbExpanded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Pins",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pins_UserId",
+                table: "Pins",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pins_Users_UserId",
+                table: "Pins",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pins_Users_UserId",
+                table: "Pins");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pins_UserId",
+                table: "Pins");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Pins");
+        }
+    }
+}
