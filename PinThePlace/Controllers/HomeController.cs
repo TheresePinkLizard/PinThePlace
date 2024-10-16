2ac35336620fe42780c3937a1fcea33cadd3ac2e
TheresePinkLizard <therese.trollbu@gmail.com>
2024-10-16 16:26:47 +0200

    prøver meg frem med table på førsteside

--- PinThePlace/Controllers/HomeController.cs
+++ PinThePlace/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using PinThePlace.ViewModels;
+using PinThePlace.Models; // Assuming your DbContext is in this namespace
+using System.Linq;
 
 namespace PinThePlace.Controllers 
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        private readonly PinDbContext _context;
+        public HomeController(PinDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+        var pins = _context.Pins.ToList();
+        var viewModel = new PinsViewModel(pins, "Home"); // Using the constructor
+        return View(viewModel);
         }
     }
 }
