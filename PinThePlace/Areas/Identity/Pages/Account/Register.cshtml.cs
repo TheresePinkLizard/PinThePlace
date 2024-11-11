13873afb05d452b711c03e192105d57635499f4b
97illimo <114436711+97illimo@users.noreply.github.com>
2024-11-11 19:25:09 +0100

    lagt til brukernavn på registreringsskjema

--- PinThePlace/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PinThePlace/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -84,6 +84,13 @@ namespace PinThePlace.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            /// 
+
+            [Required]
+            [Display(Name ="Username")]
+            public string Username {get; set;}
+
+
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
@@ -115,7 +122,7 @@ namespace PinThePlace.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
